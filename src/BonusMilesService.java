7dbbaf516c5c67418423cb54d9a15157d4ab02ea
Lazarov_O_M_QAMID_61 <oleg057orel@yandex.ru>
2023-05-10 18:56:32 +0300

    commit 2

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int bonus = 20;
+        int miles;
+        miles = price / bonus;
+        {
+            return miles;
+        }
+    }
+}
